9e1878164a2de904236ab0ea05cdd6126331569d
Magnus Holm <mail@magnusholm.com>
2017-05-21 18:25:04 +0100

    Add StyleSheet, center text

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,11 @@
-// Create a react component
+// Import
+import React, { Component } from 'react';
+import {View, Text, AppRegistry, StyleSheet } from 'react-native';
 
+// Create a react component
 var Weekdays = React.createClass({
   render: function() {
-    return <View>
+    return <View style={styles.container}>
       <Text>
         Days of the week:
       </Text>
@@ -10,6 +13,16 @@ var Weekdays = React.createClass({
   }
 });
 
-
+// Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // Moves stuff height wise
+    alignItems: 'center' // Moves stuff width wise
+  }
+});
 
 // Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
+  return Weekdays
+});
